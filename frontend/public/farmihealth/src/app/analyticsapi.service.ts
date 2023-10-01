28a759639ba7ba501881044359022e83fe1e8c24
Polokego <pomakga022@student.wethinkcode.co.za>
2023-10-01 20:49:59 +0200

    Front-End splash page

--- frontend/public/farmihealth/src/app/analyticsapi.service.ts
+++ frontend/public/farmihealth/src/app/analyticsapi.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalyticsapiService {
 
-  constructor() { }
+  constructor(http: HttpClient) { }
 }
--- frontend/public/farmihealth/src/app/authapi.service.ts
+++ frontend/public/farmihealth/src/app/authapi.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthapiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public makeNewUser(userDetails: any): void {
+
+    this.http.post("",{});
+  }
 }
