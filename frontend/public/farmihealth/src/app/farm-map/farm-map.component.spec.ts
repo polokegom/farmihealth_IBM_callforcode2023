f02ae2b0589f76225c748a92143c1aedf8d63485
Polokego <pomakga022@student.wethinkcode.co.za>
2023-10-04 09:12:10 +0200

    frront-end: maps

---
+++ frontend/public/farmihealth/src/app/farm-map/farm-map.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FarmMapComponent } from './farm-map.component';
+
+describe('FarmMapComponent', () => {
+  let component: FarmMapComponent;
+  let fixture: ComponentFixture<FarmMapComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [FarmMapComponent]
+    });
+    fixture = TestBed.createComponent(FarmMapComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
