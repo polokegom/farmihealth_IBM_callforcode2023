cd8ece5dd72327ab63030679c9064a949eb35d3f
Polokego <pomakga022@student.wethinkcode.co.za>
2023-10-06 14:51:28 +0200

    broken MAP UI

--- frontend/public/farmihealth/src/app/farm-map/farm-map.component.ts
+++ frontend/public/farmihealth/src/app/farm-map/farm-map.component.ts
@@ -6,44 +6,78 @@ import { GoogleMap } from '@angular/google-maps';
   templateUrl: './farm-map.component.html',
   styleUrls: ['./farm-map.component.css']
 })
-export class FarmMapComponent implements OnInit{
+export class FarmMapComponent implements OnInit {
+  private map: google.maps.Map | undefined; ;
+  private lassoSelecting: boolean = false;
+  private lassoPolygon!: google.maps.Polygon ;
+  private lassoCoordinates: google.maps.LatLng[] = [];
 
-  constructor() { }
 
   ngOnInit(): void {
-    this.initMap();
-  }
-
-  initMap(): void {
-
-
     const script = document.createElement('script');
     script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAWliISHIJ4PnZ73U5BQ2W9GN1_YxTuWOg&callback=initMap`;
     script.async = true;
     script.defer = true;
     script.onload = () => {
-      this.makeMap();
+      this.initMap();
     };
-    document.head.appendChild(script);
+    document.head.appendChild(script);   
   }
 
-  makeMap(): void {
+  initMap(): void {
     const mapOptions = {
-      center: { lat: -33.9249, lng: 18.4241  }, 
-      zoom: 12, 
+      center: { lat: -33.9249, lng: 18.4241 },
+      zoom: 12,
     };
     const mapElement = document.getElementById('map');
     if (mapElement) {
-      const map = new google.maps.Map(mapElement, mapOptions);
-
-      const marker = new google.maps.Marker({
-        position: { lat: -33.9249, lng: 18.4241  }, 
-        map: map,
-        title: 'Cape Town', 
-      });
-
+      this.map = new google.maps.Map(mapElement, mapOptions);
     } else {
-      console.error('Cant find map element from DOM');
+      console.error('Can\'t find map element from DOM');
     }
+  }
+
+  activateLassoSelect(): void {
+    if (!this.lassoSelecting) {
+      this.lassoSelecting = true;
+      this.lassoCoordinates = [];
+      this.createLassoPolygon();
+    }
+  }
+
+  private createLassoPolygon(): void {
+    this.lassoPolygon = new google.maps.Polygon({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      editable: false,
+      map: this.map,
+    });
+
+    google.maps.event.addListener(this.map!, 'mousemove', (event: google.maps.MapMouseEvent) => {
+      if (this.map && !this.lassoSelecting) {
+        const latLng = event.latLng;
+        if (latLng) {
+          this.lassoCoordinates.push(latLng);
+          this.lassoPolygon.getPath().push(latLng);
+        }
+      }
+    });
+
+    google.maps.event.addListener(this.map!, 'mouseup', () => {
+      if (this.map && !this.lassoSelecting) {
+        this.lassoSelecting = false;
+        // Process selected items within the lassoed area
+        // You can access the selected coordinates in this.lassoCoordinates
+        this.processSelectedItems(this.lassoCoordinates);
+      }
+    });
+  }
+
+  private processSelectedItems(selectedCoordinates: google.maps.LatLng[]): void {
+    // Implement your logic to process selected items here
+    console.log('Selected Coordinates:', selectedCoordinates);
   }
 }
--- frontend/public/farmihealth/src/app/footer/footer.component.ts
+++ frontend/public/farmihealth/src/app/footer/footer.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  @ViewChild('mapContainer') mapContainer!: ElementRef;
+  private map!: google.maps.Map;
+  private drawingManager!: google.maps.drawing.DrawingManager;
+  private isFreehandMode = false;
+  private freehandPath: google.maps.Polyline | null = null;
 
+  ngOnInit(): void {
+
+          // Load Google Maps with your API key
+          const script = document.createElement('script');
+          script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAWliISHIJ4PnZ73U5BQ2W9GN1_YxTuWOg&callback=initMap`;
+          script.async = true;
+          script.defer = true;
+          script.onload = () => {
+            // Initialize the map
+            this.initMap();
+          };
+          document.head.appendChild(script);
+        
+  }
+
+  initMap(): void {
+    const mapOptions: google.maps.MapOptions = {
+      center: { lat: -33.9249, lng: 18.4241 },
+      zoom: 12,
+    };
+    this.map = new google.maps.Map(this.mapContainer.nativeElement, mapOptions);
+    this.initDrawingManager();
+
+          // Load Google Maps with your API key
+          const script = document.createElement('script');
+          script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAWliISHIJ4PnZ73U5BQ2W9GN1_YxTuWOg&callback=initMap`;
+          script.async = true;
+          script.defer = true;
+          script.onload = () => {
+            // Initialize the map
+            this.initMap();
+          };
+          document.head.appendChild(script);
+        
+  }
+
+  initDrawingManager(): void {
+    this.drawingManager = new google.maps.drawing.DrawingManager({
+      drawingMode: null,
+      drawingControl: false,
+      drawingControlOptions: {
+        drawingModes: [google.maps.drawing.OverlayType.POLYLINE], // Use OverlayType.POLYLINE
+      },
+    });
+    this.drawingManager.setMap(this.map);
+    google.maps.event.addListener(this.drawingManager, 'overlaycomplete', (event: google.maps.drawing.OverlayCompleteEvent) => {
+      if (this.isFreehandMode && event.type === google.maps.drawing.OverlayType.POLYLINE) {
+        this.freehandPath = event.overlay as google.maps.Polyline;
+        this.toggleFreehandMode();
+      }
+    });
+  }
+
+  toggleFreehandMode(): void {
+    if (!this.isFreehandMode) {
+      this.drawingManager.setDrawingMode(google.maps.drawing.OverlayType.POLYLINE);
+    } else {
+      this.drawingManager.setDrawingMode(null);
+      if (this.freehandPath) {
+        this.freehandPath.setMap(this.map);
+      }
+    }
+    this.isFreehandMode = !this.isFreehandMode;
+  }
 }
