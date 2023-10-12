224949c6c2d3a8a9cbcaba7969522e3111cc4dbb
Polokego <pomakga022@student.wethinkcode.co.za>
2023-10-12 21:31:38 +0200

    list of views

---
+++ frontend/public/farmihealth/src/app/view-schedule-list/view-schedule-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-schedule-list',
+  templateUrl: './view-schedule-list.component.html',
+  styleUrls: ['./view-schedule-list.component.css']
+})
+export class ViewScheduleListComponent {
+
+}
