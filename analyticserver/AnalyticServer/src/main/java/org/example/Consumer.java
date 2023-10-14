d82cc54f3ecfb5952e69c7d581e1e23647e4542f
Polokego <pomakga022@student.wethinkcode.co.za>
2023-10-14 03:39:41 +0200

    Set Kafka producer, consumer

--- analyticserver/AnalyticServer/src/main/java/org/example/Consumer.java
+++ analyticserver/AnalyticServer/src/main/java/org/example/Consumer.java
@@ -1,18 +1,9 @@
 package org.example;
-/*
 
+public class Consumer {
 
-
- */
-
-public class Main {
-
-
-    
     public static void main(String[] args) {
 
-
-
-
     }
+    
 }
--- analyticserver/AnalyticServer/src/main/java/org/example/Producer.java
+++ analyticserver/AnalyticServer/src/main/java/org/example/Producer.java
@@ -1,18 +1,8 @@
 package org.example;
-/*
 
-
-
- */
-
-public class Main {
-
-
+public class Producer {
     
     public static void main(String[] args) {
-
-
-
 
     }
 }
